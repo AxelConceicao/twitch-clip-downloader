44b3fcb90993a312af3604e610ffef59ccb2164a
Axel Conceicao <axel.conceicao@epitech.eu>
2020-11-02 21:03:38 +0100

    Add confirm box when downloading clips from clips manager

--- dashboard.js
+++ dashboard.js
@@ -44,6 +44,95 @@ function getFileName(context) {
   return filename
 }
 
+function Confirm() {
+  /*change*/
+  var $content =
+  `
+  <div class="ReactModalPortal">
+    <div
+      class="ReactModal__Overlay ReactModal__Overlay--after-open modal__backdrop js-modal-backdrop"
+    >
+      <div
+        class="ReactModal__Content ReactModal__Content--after-open modal__content"
+        tabindex="-1"
+        role="dialog"
+        aria-label="Modal"
+        aria-labelledby="modal-root-header"
+        aria-describedby="modal-description-id"
+      >
+        <div
+          class="modal-wrapper__backdrop modal-wrapper__backdrop--info tw-align-items-start tw-flex tw-full-height tw-full-width tw-justify-content-center"
+          data-test-selector="modal-wrapper__backdrop-test"
+        >
+          <div
+            class="modal-wrapper__content modal-wrapper__content--info tw-flex tw-flex-grow-0 tw-full-width tw-justify-content-center tw-relative"
+            data-test-selector="modal-wrapper__content-test"
+          >
+            <div>
+              <div class="clip-modal tw-c-background-base tw-pd-2">
+                <div data-test-selector="clips-modal-title" class="tw-border-b tw-mg-b-2 tw-pd-b-1">
+                  <h4 id="modal-root-header" class="">Download these clips?</h4>
+                </div>
+                <div
+                  data-test-selector="clips-modal-main-body"
+                  class="tw-border-b tw-mg-b-2 tw-pd-b-2"
+                >
+                  <p class="tw-mg-b-1">` + parseInt(countSelectedClips()) + ` clips will be downloaded.</p>
+                  <p class="tw-mg-b-1">For more than ten clips I strongly recommend you to disable the setting
+                  <strong>Always ask you where to save files</strong>.</p>
+                  <a target="_blank" rel="noopener noreferrer" href="https://justpaste.it/9hypq" style="cursor: pointer;">See instructions here</a>
+                </div>
+                <div class="tw-flex tw-justify-content-center">
+                  <div class="tw-mg-x-1">
+                    <button
+                      class="cancelActionTCD tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-core-button tw-core-button--text tw-inline-flex tw-interactive tw-justify-content-center tw-overflow-hidden tw-relative"
+                      data-test-selector="clips-modal-cancel-button"
+                    >
+                      <div class="tw-align-items-center tw-core-button-label tw-flex tw-flex-grow-0">
+                        <div data-a-target="tw-core-button-label-text" class="tw-flex-grow-0">
+                          Cancel
+                        </div>
+                      </div>
+                    </button>
+                  </div>
+                  <div class="tw-mg-x-1">
+                    <button
+                      class="doActionTCD tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-core-button tw-core-button--primary tw-inline-flex tw-interactive tw-justify-content-center tw-overflow-hidden tw-relative"
+                      data-test-selector="clips-modal-submit-button"
+                    >
+                      <div class="tw-align-items-center tw-core-button-label tw-flex tw-flex-grow-0">
+                        <div data-a-target="tw-core-button-label-text" class="tw-flex-grow-0">
+                          Download
+                        </div>
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
+  $('body').prepend($content)
+  $('.doActionTCD').click(function () {
+    downloadSelectedClips()
+    $(this)
+      .parents('.ReactModalPortal')
+      .fadeOut(500, function () {
+        $(this).remove()
+      })
+  })
+  $('.cancelActionTCD').click(function () {
+    $(this)
+      .parents('.ReactModalPortal')
+      .fadeOut(500, function () {
+        $(this).remove()
+      })
+  })
+}
 function downloadSelectedClips() {
   rows = document.getElementsByClassName('clmgr-table__row')
   for (var i = 0; i < rows.length; i++) {
@@ -63,7 +152,7 @@ function addDownloadButton() {
   context.insertAdjacentHTML(
     'beforeend',
     `
-      <div class="tw-mg-l-1 th-download-btn">
+      <div class="tw-mg-l-1 th-download-btn button-default link">
         <button
           class="tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-core-button tw-core-button--primary tw-inline-flex tw-interactive tw-justify-content-center tw-overflow-hidden tw-relative"
         >
@@ -77,7 +166,14 @@ function addDownloadButton() {
       `
   )
   dlBtn = document.getElementsByClassName('th-download-btn')[0].getElementsByTagName('button')[0]
-  dlBtn.onclick = downloadSelectedClips
+  dlBtn.onclick = function () {
+    var nbClips = parseInt(countSelectedClips())
+    if (nbClips > 10) {
+      Confirm()
+    } else {
+      downloadSelectedClips()
+    }
+  }
 }
 
 function setListener() {
